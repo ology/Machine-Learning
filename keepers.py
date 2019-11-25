33672ecca2f5fc3106aba83ccbc5ed8865d84134
Gene Boggs <gene@ology.net>
2019-11-25 09:25:49 -0800

    Moved from personal devel repo

---
+++ keepers.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+
+data = pd.read_csv('keepers.csv', sep='\t')
+
+data.fillna(value=0, inplace=True)
+
+pd.set_option('display.max_rows', data.shape[0]+1)
+
+#print(data)
+#print(data.head())
+#print(data.tail())
+#print(data.shape)
+
+feature_cols = ['age', 'seasons', 'mins', 'apps', 'minapp', 'starts', 'subs', 'ga', 'ga90', 'sota', 'win', 'draw', 'loss', 'cs', 'csperc', 'saves']
+response_col = ['rank']
+
+#sns.pairplot(data, x_vars=feature_cols, y_vars=response_col, size=7, aspect=0.7, kind='reg')
+#plt.show()
+
+X = data[feature_cols]
+y = data[response_col]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+
+#print(X_train)
+#print(X_test)
+#print(y_train)
+#print(X_train.shape, y_train.shape)
+#print(X_test.shape, y_test.shape)
+#print(np.isnan(X_train))
+
+estimator = LinearRegression()
+estimator.fit(X_train, y_train)
+
+#print(estimator.intercept_)
+#print(estimator.coef_)
+#zip(feature_cols, estimator.coef_[0])
+
+y_pred = estimator.predict(X_test)
+
+print(metrics.mean_absolute_error(y_test, y_pred))
+print(metrics.mean_squared_error(y_test, y_pred))
+print(np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
