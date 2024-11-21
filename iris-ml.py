951efbe05f3611c776c46146019ca4cefa065a69
Gene Boggs <gene@ology.net>
2024-11-21 00:56:31 -0800

    WIP

--- iris-ml.py
+++ iris-ml.py
@@ -24,3 +24,10 @@ dataset['class'] = [ class_names[x] for x in iris.target ]
 # print(dataset.shape)
 # print(dataset.describe())
 # print(dataset.groupby('class').size())
+
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+plt.show()
+dataset.hist()
+plt.show()
+scatter_matrix(dataset)
+plt.show()
