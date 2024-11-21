3e34d614466df31e74682e972781de06b3e2e741
Gene Boggs <gene@ology.net>
2024-11-21 10:51:08 -0800

    WIP

--- chesspos.py
+++ chesspos.py
@@ -13,7 +13,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import KFold
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
@@ -102,7 +102,7 @@ if __name__ == "__main__":
     results = []
     names = []
     for name, model in models:
-        kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+        kfold = KFold(n_splits=10, random_state=1, shuffle=True)
         cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
         results.append(cv_results)
         names.append(name)
