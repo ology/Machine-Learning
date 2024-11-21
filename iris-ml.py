c582f7e8c819b14af46b88d9d26dccccdcef5332
Gene Boggs <gene@ology.net>
2024-11-21 01:18:50 -0800

    WIP

--- iris-ml.py
+++ iris-ml.py
@@ -35,17 +35,17 @@ dataset['class'] = [ class_names[x] for x in iris.target ]
 values = dataset.values
 X = values[:, 0:4]
 y = values[:, 4]
-X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.20, random_state=1)
 
 # inspect algorithms
 models = []
-models.append(('LR', LogisticRegression(solver='liblinear')))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
+models.append(('  LR', LogisticRegression(solver='liblinear')))
+models.append((' LDA', LinearDiscriminantAnalysis()))
+models.append((' KNN', KNeighborsClassifier()))
 models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC(gamma='auto')))
-# evaluate each model in turn
+models.append(('  NB', GaussianNB()))
+models.append((' SVM', SVC(gamma='auto')))
+print('Evaluate algorithms:')
 results = []
 names = []
 for name, model in models:
@@ -53,4 +53,15 @@ for name, model in models:
 	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
 	results.append(cv_results)
 	names.append(name)
-	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+	print('%s: mean=%f (std=%f)' % (name, cv_results.mean(), cv_results.std()))
+# plt.boxplot(results, labels=names)
+# plt.title('Algorithm Comparison')
+# plt.show()
+
+# prediction
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_test)
+print('\nAccuracy:', accuracy_score(Y_test, predictions))
+print('\nConfusion:\n', confusion_matrix(Y_test, predictions))
+print('\nClassification:\n', classification_report(Y_test, predictions))
