fe69c4455cbc1134e6370d8359b2697eb0595a51
Gene Boggs <gene@ology.net>
2024-11-22 11:27:00 -0800

    WIP

--- iris-ml.py
+++ iris-ml.py
@@ -39,38 +39,38 @@ dataset['class'] = [ class_names[x] for x in iris.target ]
 # scatter_matrix(dataset)
 # plt.show()
 
-# values = dataset.values
-# X = values[:, 0:4]
-# # print(X)
-# y = values[:, 4]
-# # print(y)
-# X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.20, random_state=1)
+values = dataset.values
+X = values[:, 0:4]
+# print(X)
+y = values[:, 4]
+# print(y)
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.20, random_state=1)
 
-# # inspect algorithms
-# models = []
-# models.append(('  LR', LogisticRegression(solver='liblinear')))
-# models.append((' LDA', LinearDiscriminantAnalysis()))
-# models.append((' KNN', KNeighborsClassifier()))
-# models.append(('CART', DecisionTreeClassifier()))
-# models.append(('  NB', GaussianNB()))
-# models.append((' SVM', SVC(gamma='auto')))
-# print('Evaluate algorithms:')
-# results = []
-# names = []
-# for name, model in models:
-# 	kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-# 	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
-# 	results.append(cv_results)
-# 	names.append(name)
-# 	print('%s: mean=%f (std=%f)' % (name, cv_results.mean(), cv_results.std()))
-# # plt.boxplot(results, labels=names)
-# # plt.title('Algorithm Comparison')
-# # plt.show()
+# inspect algorithms
+models = []
+models.append(('  LR', LogisticRegression(solver='liblinear')))
+models.append((' LDA', LinearDiscriminantAnalysis()))
+models.append((' KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('  NB', GaussianNB()))
+models.append((' SVM', SVC(gamma='auto')))
+print('Evaluate algorithms:')
+results = []
+names = []
+for name, model in models:
+	kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+	results.append(cv_results)
+	names.append(name)
+	print('%s: mean=%f (std=%f)' % (name, cv_results.mean(), cv_results.std()))
+# plt.boxplot(results, labels=names)
+# plt.title('Algorithm Comparison')
+# plt.show()
 
-# # prediction
-# model = SVC(gamma='auto')
-# model.fit(X_train, Y_train)
-# predictions = model.predict(X_test)
-# print('\nAccuracy:', accuracy_score(Y_test, predictions))
-# # print('\nConfusion:\n', confusion_matrix(Y_test, predictions))
-# # print('\nClassification:\n', classification_report(Y_test, predictions))
+# prediction
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_test)
+print('\nAccuracy:', accuracy_score(Y_test, predictions))
+# print('\nConfusion:\n', confusion_matrix(Y_test, predictions))
+# print('\nClassification:\n', classification_report(Y_test, predictions))
