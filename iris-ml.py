16e9fa452c8971762bb40959aae5b9c3cd865262
Gene Boggs <gene@ology.net>
2024-11-22 16:52:39 -0800

    WIP

--- iris-ml.py
+++ iris-ml.py
@@ -1,16 +1,12 @@
 from matplotlib import pyplot as plt
 import pandas as pd
-from pandas import read_csv
 from pandas.plotting import scatter_matrix
 import seaborn as sns
 from sklearn import datasets
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import StratifiedKFold
 from sklearn.naive_bayes import GaussianNB
