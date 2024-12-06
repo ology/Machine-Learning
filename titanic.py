1295f3bafc1da6fc73d5fd179b727eb6a1447083
Gene Boggs <gene@ology.net>
2024-12-06 02:13:07 -0800

    WIP

--- titanic.py
+++ titanic.py
@@ -6,7 +6,7 @@
 import pandas as pd
 # import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression
 # from sklearn.metrics import accuracy_score, precision_score, recall_score
 import re
 import string
@@ -76,7 +76,10 @@ df3.loc[df3['Embarked'] == 'S', 'Embarked'] = 'Southampton'
 df3.loc[df3['Embarked'] == 'Q', 'Embarked'] = 'Queenstown'
 df3.loc[df3['Sex'] == 'male', 'Sex'] = 0
 df3.loc[df3['Sex'] == 'female', 'Sex'] = 1
-# print(df3.columns)
+df3.loc[df3['SibSp'].isna(), 'SibSp'] = 0
+df3.loc[df3['Parch'].isna(), 'Parch'] = 0
+df3.loc[df3['Fare'].isna(), 'Fare'] = 0
+# print(df3)
 # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 #     print(df3['Embarked'])
 
@@ -91,19 +94,15 @@ df4.loc[df4['Home'] == -1, 'Home'] = pd.NA
 # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
 #     print(df4)
 
-# Split the data into training and test sets
 X = df4.drop(['Survived', 'Name', 'Ticket', 'Cabin'], axis=1)
 y = df4['Survived']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# # Build the model
 # model = LogisticRegression()
 # model.fit(X_train, y_train)
 
-# # Make predictions on the test set
 # y_pred = model.predict(X_test)
 
-# # Evaluate the model
 # accuracy = accuracy_score(y_test, y_pred)
 # precision = precision_score(y_test, y_pred)
 # recall = recall_score(y_test, y_pred)
